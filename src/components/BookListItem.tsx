ae6bc6fe9ae6a80b46f890bfdf188138d6f394e5
Gabriel Heinrich <gabriel@codecampleipzig.de>
2022-11-25 21:03:56 +0100

    06 Display a money bag emoji next to free books

--- src/components/BookListItem.tsx
+++ src/components/BookListItem.tsx
@@ -4,11 +4,27 @@ export interface BookListItemProps {
   book: Book;
 }
 
+const getPriceRating = (price: string) => {
+  const priceNumber = parseInt(price.substring(1));
+  if (isNaN(priceNumber)) return "";
+  if (priceNumber <= 10) return "$";
+  if (priceNumber <= 30) return "$$";
+  return "$$$";
+};
+
 export const BookListItem = ({ book }: BookListItemProps) => {
+  const isFree = book.price === "$0.00";
+
   return (
-    <div className="book-list-item">
-      <h2>{book.title}</h2>
+    <div className={`book-list-item ${isFree ? "book-list-item_free" : ""}`}>
+      <h2>
+        {isFree && <span>💰 </span>}
+        {book.title}
+      </h2>
       <h3>{book.subtitle}</h3>
+      {!isFree && (
+        <div style={{ color: "green" }}>{getPriceRating(book.price)}</div>
+      )}
       <div className="text-meta">by {book.author}</div>
     </div>
   );
