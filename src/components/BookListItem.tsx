55b815f1d6e1b43ca9d276c27bafe9f27c6114d1
Gabriel Heinrich <gabriel@codecampleipzig.de>
2022-11-25 20:24:21 +0100

    04 Create a BookListItem component

---
+++ src/components/BookListItem.tsx
@@ -0,0 +1,15 @@
+import { Book } from "../domain/book/Book";
+
+export interface BookListItemProps {
+  book: Book;
+}
+
+export const BookListItem = ({ book }: BookListItemProps) => {
+  return (
+    <div className="book-list-item">
+      <h2>{book.title}</h2>
+      <h3>{book.subtitle}</h3>
+      <div className="text-meta">by {book.author}</div>
+    </div>
+  );
+};
