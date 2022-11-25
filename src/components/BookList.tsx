e68c63938d96a963bfa1e49319404009a1975b1b
Gabriel Heinrich <gabriel@codecampleipzig.de>
2022-11-25 20:24:21 +0100

    05 Display a list of books

---
+++ src/components/BookList.tsx
@@ -0,0 +1,31 @@
+import type { Book } from "../domain/book/Book";
+import { BookListItem } from "./BookListItem";
+
+interface BookListProps {
+  books: Book[];
+}
+
+const getMinPages = (books: Book[]) => {
+  return books.reduce((minPages, book) => {
+    return Math.min(minPages, book.numPages);
+  }, Number.MAX_SAFE_INTEGER);
+};
+
+const getMaxPages = (books: Book[]) => {
+  // We can also use the arrow function shorthand for reduce (see getMinPages for the longer syntax)
+  return books.reduce((maxPages, book) => Math.max(maxPages, book.numPages), 0);
+};
+
+export const BookList = ({ books }: BookListProps) => {
+  return (
+    <div className="book-list">
+      <small className="text-meta">
+        Showing {books.length} books with {getMinPages(books)} to{" "}
+        {getMaxPages(books)} pages.
+      </small>
+      {books.map((b) => (
+        <BookListItem key={b.id} book={b} />
+      ))}
+    </div>
+  );
+};
