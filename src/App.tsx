91db141fab27c33bb84b27bdbb1ade1cab7d5efe
Gabriel Heinrich <gabriel@codecampleipzig.de>
2022-11-26 11:30:11 +0100

    09 Fetch and display books from the Bookmonkey API

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import { AppHeader } from "./components/AppHeader";
 import { BookList } from "./components/BookList";
-import { exampleBooks } from "./domain/book/exampleBooks";
+import { fetchBooks } from "./domain/book/api";
+import { Book } from "./domain/book/Book";
+
+type FetchState = "initial" | "loading" | "success" | "error";
 
 function App() {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [state, setState] = useState<FetchState>("initial");
+
+  useEffect(() => {
+    setState("loading");
+    fetchBooks()
+      .then((books) => {
+        setBooks(books);
+        setState("success");
+      })
+      .catch(() => {
+        setState("error");
+      });
+  }, []);
+
   return (
     <div className="App">
       <AppHeader />
-      <BookList books={exampleBooks} />
+      {state === "loading" && (
+        <h2 className="text-meta m-top">Loading books...</h2>
+      )}
+      {state === "error" && (
+        <h2 className="error m-top">Error while loading books...</h2>
+      )}
+      {state === "success" && <BookList books={books} />}
     </div>
   );
 }
---
+++ src/domain/book/api.ts
@@ -0,0 +1,7 @@
+import { Book } from "./Book";
+
+export const fetchBooks = () => {
+  return fetch(`${import.meta.env.VITE_REACT_APP_URL_API}/books`).then((res) =>
+    res.json(),
+  ) as Promise<Book[]>;
+};
