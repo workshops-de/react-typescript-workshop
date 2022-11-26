13d43c81a97651e666c4d9f6c1971be0609df322
Gabriel Heinrich <gabriel@codecampleipzig.de>
2022-11-26 16:59:23 +0100

    15 Add navigation links to the Books and About Screen

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,15 +1,27 @@
 import { usePrimaryColor } from "../domain/theme/hooks";
 import logo from "../images/logo.png";
 import { ThemeEditor } from "./ThemeEditor";
+import { NavLink } from "react-router";
 
 export const AppHeader = () => {
   const primaryColor = usePrimaryColor();
+
+  const highlightActiveLink = ({ isActive }: { isActive: boolean }) => ({
+    color: isActive ? primaryColor : "",
+  });
 
   return (
     <header className="app-header">
       <img src={logo} alt="Awesome comic-style monkey with sunglasses" />
       <h1 style={{ color: primaryColor }}>Bookmonkey</h1>
       <ThemeEditor />
+
+      <nav style={{ "--primary-600": primaryColor } as React.CSSProperties}>
+        <NavLink to="/books">Books</NavLink>
+        <NavLink to="/about" style={highlightActiveLink}>
+          About
+        </NavLink>
+      </nav>
     </header>
   );
 };
