fea4ad5ed7cabade614b1040f75abc5a4734d460
Gabriel Heinrich <gabriel@codecampleipzig.de>
2022-11-26 12:21:25 +0100

    12 Create a ThemeEditor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AppHeader } from "./components/AppHeader";
 import { BookList } from "./components/BookList";
 import { useBooks } from "./domain/book/hooks";
+import { Theme } from "./domain/theme/Theme";
+import { ThemeContext } from "./domain/theme/ThemeContext";
 
 function App() {
   const { books, state, error, refresh } = useBooks();
+  const [primaryColor, setPrimaryColor] = useState("tomato");
+
+  const theme: Theme = {
+    primaryColor,
+    setPrimaryColor,
+  };
 
   useEffect(() => {
     const intervalId = setInterval(refresh, 30000);
@@ -12,14 +20,16 @@ function App() {
   });
 
   return (
-    <div className="App">
-      <AppHeader />
-      {state === "loading" && (
-        <h2 className="text-meta m-top">Loading books...</h2>
-      )}
-      {state === "error" && <h2 className="error m-top">{error.message}</h2>}
-      {state === "success" && <BookList books={books} />}
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <div className="App">
+        <AppHeader />
+        {state === "loading" && (
+          <h2 className="text-meta m-top">Loading books...</h2>
+        )}
+        {state === "error" && <h2 className="error m-top">{error.message}</h2>}
+        {state === "success" && <BookList books={books} />}
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
