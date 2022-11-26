7096bb2be4baf044ca7729875fa45e35ed96c7ed
Gabriel Heinrich <gabriel@codecampleipzig.de>
2022-11-26 12:21:25 +0100

    11 Add a ThemeContext to provide a primary color

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,10 +1,13 @@
+import { usePrimaryColor } from "../domain/theme/hooks";
 import logo from "../images/logo.png";
 
 export const AppHeader = () => {
+  const primaryColor = usePrimaryColor();
+
   return (
     <header className="app-header">
       <img src={logo} alt="Awesome comic-style monkey with sunglasses" />
-      <h1>Bookmonkey</h1>
+      <h1 style={{ color: primaryColor }}>Bookmonkey</h1>
     </header>
   );
 };
