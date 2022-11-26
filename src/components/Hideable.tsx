26f361e70b9c8d150888fb9a52f2948b9242a0fc
Gabriel Heinrich <gabriel@codecampleipzig.de>
2022-11-26 10:49:52 +0100

    08 Display the book's abstract, but make it hideable

---
+++ src/components/Hideable.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useState } from "react";
+
+interface HideableProps {
+  initiallyVisible?: boolean;
+  children: ReactNode;
+}
+
+export const Hideable: React.FC<HideableProps> = ({
+  initiallyVisible,
+  children,
+}) => {
+  const [visible, setVisible] = useState(initiallyVisible);
+
+  const handleToggle = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div className="hideable">
+      {visible && children}
+      <button className="tertiary" onClick={handleToggle}>
+        {visible ? "- hide" : "+ show"} details
+      </button>
+    </div>
+  );
+};
