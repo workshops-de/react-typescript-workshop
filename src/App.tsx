cfa8f2dd30e5beb90fd426d590642ac8b88779dd
André Kovac <me@andrekovac.com>
2022-12-14 10:31:07 +0100

    21 Install Redux and create store

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import { Provider } from "react-redux";
 import { Outlet } from "react-router-dom";
+
 import "./App.css";
+
 import { AppHeader } from "./components/AppHeader";
 import { ThemeEditor } from "./components/ThemeEditor";
 import { ThemeContext, Theme } from "./domain/theme";
+import store from "./store";
 
 function App() {
   const [primaryColor, setPrimaryColor] = useState("tomato");
@@ -13,13 +17,15 @@ function App() {
   };
 
   return (
-    <ThemeContext.Provider value={theme}>
-      <div className="App">
-        <ThemeEditor />
-        <AppHeader />
-        <Outlet />
-      </div>
-    </ThemeContext.Provider>
+    <Provider store={store}>
+      <ThemeContext.Provider value={theme}>
+        <div className="App">
+          <ThemeEditor />
+          <AppHeader />
+          <Outlet />
+        </div>
+      </ThemeContext.Provider>
+    </Provider>
   );
 }
 
