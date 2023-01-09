4123c6d37029e06ff896e5a9ebc11d7283af9a1f
André Kovac <me@andrekovac.com>
2023-01-09 23:17:52 +0100

    24 component tests

---
+++ src/__tests__/components/BookListItem.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { BookListItem } from "../../components/BookListItem";
+import { Book } from "../../domain/book/Book";
+
+describe("components/BookListItem", () => {
+  const book = {
+    id: "1",
+    title: "My book",
+    isbn: "my-isbn",
+    price: "$11.11",
+  } as Book;
+
+  test("renders book with title", () => {
+    render(<BookListItem book={book} />, {
+      wrapper: MemoryRouter,
+    });
+
+    expect(screen.getByText(book.title)).toBeTruthy();
+  });
+});
